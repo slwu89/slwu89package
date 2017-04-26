3cde892e615b8ee8f005caa835d4d1af152698d3
slwu89 <slwu89@berkeley.edu>
2017-04-26 12:12:37 -0700

    GSL sample indices

--- src/samplePermute.cpp
+++ src/samplePermute.cpp
@@ -61,3 +61,54 @@ IntegerVector gslShuffle(int seed, int size){
 
   return(out);
 }
+
+// [[Rcpp::export]]
+IntegerVector multinomGSL(int seed, unsigned int N, NumericVector probs){
+  
+  gsl_rng *r = gsl_rng_alloc(gsl_rng_mt19937);
+  gsl_rng_set(r,seed);
+  
+  size_t K = probs.size(); // number of categories
+  
+  unsigned int sample[K]; // array of counts
+  gsl_ran_multinomial(r,K,N,probs.begin(),sample); // sample
+  
+  IntegerVector out(K); // format output as IntegerVector
+  for(int i=0; i<K; i++){
+    out[i] = sample[i];
+  }
+  
+  
+  return(out);
+}
+
+// [[Rcpp::export]]
+IntegerVector sampleGSL(int seed, int x, NumericVector probs){
+  
+  gsl_rng *r = gsl_rng_alloc(gsl_rng_mt19937);
+  gsl_rng_set(r,seed);
+  
+  size_t K = int(x);
+  
+  gsl_ran_discrete_t * sampler;
+  sampler = gsl_ran_discrete_preproc(K, probs.begin());
+  
+  size_t ii;
+  IntegerVector out(x);
+  
+  for(int i=0; i<x; i++){
+    ii = gsl_ran_discrete(r,sampler);
+    out[i] = ii;
+  }
+  
+  gsl_ran_discrete_free(sampler);
+  gsl_rng_free(r);
+  
+  return(out);
+}
+
+
+
+
+
+
